0f5a802bb3f2d53cccde505d20783cc3ca09d2ad
jaden ficklin <jadendficklin@gmail.com>
2022-11-04 15:27:05 -0600

    imported list of hooks to learn

---
+++ src/components/HooksPracticeOne.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function HooksPracticeOne() {
+  return (
+    <>
+    <h1>list of hooks</h1>
+    <ol>
+        <li>useState</li>
+        <li>useEffect</li>
+        <li>useContext</li>
+        <li>useReducer</li>
+        <li>useCallback</li>
+        <li>useMemo</li>
+        <li>useRef</li>
+        <li>useImperitiveHandle</li>
+        <li>useLayout</li>
+        </ol> 
+
+    </>
+  )
+}
+
+export default HooksPracticeOne
